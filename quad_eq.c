1672445e38161b66d5a07c581b28d29efafd82f7
TheCreativeSpark <134577077+thecreativespark@users.noreply.github.com>
2023-11-02 19:33:41 +0530

    check for invalid user input

--- quad_eq.c
+++ quad_eq.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main(){
-float a,b,c,d,r1,r2;
+float a,b,c,d,r1,r2, realp, imagp;
 printf("\nTo find the root of a Quadratic Equation in the form ax^2+bx+c=0\n");
 printf("Enter the \"a\" term in the equation: \n");
 scanf("%f", &a);
@@ -11,9 +11,12 @@ scanf("%f", &b);
 printf("Enter the \"c\" term in the equation: \n");
 scanf("%f", &c);
 
-d=(b*b)-4*a*c;
-
-if(d > 0){
+if(a==0 || sizeof(a)!=sizeof(float) ){
+    printf("Invalid value for a\n");
+}
+else{
+    d=(b*b)-4*a*c;
+    if(d > 0){
     r1 = ((-b)+sqrt(d))/(2*a);
     r2 = ((-b)-sqrt(d))/(2*a);
     printf("\nThe first root is %.2f\n", r1);
@@ -25,7 +28,13 @@ else if(d == 0){
 }
 else{
     printf("\nGiven equation has imaginary roots.\n");
+    realp = (-b)/(2*a);
+    imagp = d/(2*a);
+    printf("(%.3f)+i(%.3f)\n", realp, imagp);
+    printf("(%.3f)-i(%.3f)\n", realp, imagp);
 }
+}
+
 printf("\n");
 return 0;
 }
