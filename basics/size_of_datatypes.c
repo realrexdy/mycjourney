68696e3f7846274c41f906af73cac93b337271d0
TheCreativeSpark <guneshreddys0202@gmail.com>
2023-12-15 16:02:38 +0530

    refresh

---
+++ basics/size_of_datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a=0;
+    char b=0;
+    float c=0;
+    double d=0;
+    printf("%d\n", sizeof(a));
+    printf("%d\n",sizeof(b));
+    printf("%d\n", sizeof(c));
+    printf("%d\n", sizeof(d));
+}
--- basics/variable_swap.c
+++ basics/variable_swap.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(){
+    
     // Variable swap using temp variable
-    int a,b,temp;
+    /*int a,b,temp;
     printf("Enter the first variable:\n");
     scanf("%d",&a);
     printf("Enter the second variable:\n");
@@ -12,5 +13,18 @@ int main(){
     b = temp;
     printf("a=%d\n", a);
     printf("b=%d\n", b);
+    return 0;*/
+
+    //Variable swap without using a temp variable
+    float a,b;
+    printf("Enter the first variable:\n");
+    scanf("%f",&a);
+    printf("Enter the second variable:\n");
+    scanf("%f",&b);
+    a=a/b;
+    b=a*b;
+    a=b/a;
+    printf("a=%.2f\n", a);
+    printf("b=%.2f\n", b);
     return 0;
-}
+    }
