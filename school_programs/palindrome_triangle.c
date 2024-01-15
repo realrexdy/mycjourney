315d9d5f2cd65938563ba8ea0bae60a10ea2137c
TheCreativeSpark <thecreativespark@proton.me>
2024-01-15 19:25:11 +0530

    palindrome triangle

---
+++ school_programs/palindrome_triangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    printf("Enter the value of N: ");
+    int n;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            printf(" ");
+        }
+        for(int j=1; j<=i; j++){
+            printf("%d", j);
+        }
+        for(int j=i-1; j>0; j--){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
