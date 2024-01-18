8135dd3262b6657cd68df2ab3be2ff07e5d8d10b
TheCreativeSpark <thecreativespark@proton.me>
2024-01-18 17:28:50 +0530

    binser

---
+++ vtu-I-sem-lab/4.c
@@ -0,0 +1,22 @@
+/*This is a program to print a Palindrome triangle of n rows.*/
+
+#include <stdio.h>
+
+int main(){
+    printf("\nEnter the value of n\n");
+    int n;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            printf(" ");
+        }
+        for(int j=1; j<=i; j++){
+            printf("%d", j);
+        }
+        for(int j=i-1; j>0; j--){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ vtu-I-sem-lab/5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int binser(int a[], int key, int left, int right){
+    int mid = left + ((right-left)/2);
+    
+    if(left<=right){
+        if(a[mid]>key){
+        return binser(a, key, left, mid-1);
+    }
+    else if(a[mid]<key){
+        return binser(a, key, mid+1, right);
+    }
+    else{
+        return mid;
+    }
+    }
+    else{
+        return -69;
+    }
+
+}
+
+int main(){
+    int a[1000], key, left=0, right;
+    printf("Enter the number of elements in the array:");
+    scanf("%d", &right);
+    printf("\nEnter the elements of the array:\n");
+    for(int i=0; i<right; i++){
+        scanf("%d", &a[i]);
+    }
+    printf("\nEnter the key element:");
+    scanf("%d", &key);
+    int keyind=binser(a, key, left, right);
+    (keyind==-69)?printf("Key element not found in the array."):printf("\nThe key element is found at %dth position.\n", keyind+1);
+    return 0;
+}
